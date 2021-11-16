83c8b68123f49daaad9ff5e711db99e35673f086
JacobPaxton <jacob.paxton65@gmail.com>
2021-11-16 11:50:14 -0600

    add revised model creation script

--- model.py
+++ model.py
@@ -1,8 +1,5 @@
 import pandas as pd
-from sklearn.model_selection import train_test_split
 
-from sklearn.feature_extraction.text import CountVectorizer
-from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.linear_model import LogisticRegression
@@ -33,8 +30,8 @@ def print_nlp_results(y_insample, y_outsample):
 
 # -------------------------- the shotgun -------------------------- #
 
-def nlp_shotgun(X_insample, y_insample, X_outsample, y_outsample):
-    """ Take in Pandas Series for NLP content and target (pass columns!),
+def classification_shotgun(X_insample, y_insample, X_outsample, y_outsample):
+    """ Take in Pandas Series for classification and target (pass columns!),
         Create several DecisionTree, RandomForest, LogisticRegression, Naive Bayes,
         and KNearest classification models, 
         Push model predictions to originating dataframe, return dataframe """
@@ -44,7 +41,7 @@ def nlp_shotgun(X_insample, y_insample, X_outsample, y_outsample):
     if type(y_outsample) != 'pandas.core.frame.DataFrame':
         y_outsample = pd.DataFrame(y_outsample.rename('out_actuals'))
     # Baseline - add predictions to df
-    y_insample, y_outsample = nlp_bl(y_insample, y_outsample)
+    y_insample, y_outsample = classification_bl(y_insample, y_outsample)
     # Decision Tree classifier - add predictions to df
     y_insample, y_outsample = decisiontree(X_insample, y_insample, X_outsample, y_outsample)
     # Random Forest classifier - add predictions to df
@@ -60,7 +57,7 @@ def nlp_shotgun(X_insample, y_insample, X_outsample, y_outsample):
 
 # -------------------------- the models -------------------------- #
 
-def nlp_bl(y_insample, y_outsample):
+def classification_bl(y_insample, y_outsample):
     """ Calculate baseline using mode class for model comparison """
     # find baseline
     mode = y_insample.in_actuals.mode().item()
@@ -72,148 +69,71 @@ def nlp_bl(y_insample, y_outsample):
 
 def decisiontree(X_insample, y_insample, X_outsample, y_outsample):
     """ Creates decision trees with max_depth 1,2,3,5,10 and random_state=123 """
-    # transform data into count-vectorized and TFIDF-vectorized data
-    X_cv_insample, X_cv_outsample = count_vectorizer(X_insample, X_outsample)
-    X_tfidf_insample, X_tfidf_outsample = tfidf(X_insample, X_outsample)
     # set loop list
     max_depths = [1,2,3,5,10]
     # loop through max depths
     for depth in max_depths:
-        # create trees for count-vectorized and TFIDF-vectorized documents
-        tree_cv = DecisionTreeClassifier(max_depth=depth, random_state=123)\
-            .fit(X_cv_insample, y_insample.in_actuals)
-        tree_tfidf = DecisionTreeClassifier(max_depth=depth, random_state=123)\
-            .fit(X_tfidf_insample, y_insample.in_actuals)
+        # create decision trees
+        tree = DecisionTreeClassifier(max_depth=depth, random_state=123)\
+            .fit(X_insample, y_insample.in_actuals)
         # make predictions in new columns
-        y_insample['cv_tree_maxdepth' + str(depth)] = tree_cv.predict(X_cv_insample)
-        y_outsample['cv_tree_maxdepth' + str(depth)] = tree_cv.predict(X_cv_outsample)
-        y_insample['tfidf_tree_maxdepth' + str(depth)] = tree_tfidf.predict(X_tfidf_insample)
-        y_outsample['tfidf_tree_maxdepth' + str(depth)] = tree_tfidf.predict(X_tfidf_outsample)
+        y_insample['cv_tree_maxdepth' + str(depth)] = tree.predict(X_insample)
+        y_outsample['cv_tree_maxdepth' + str(depth)] = tree.predict(X_outsample)
 
     return y_insample, y_outsample # return dataframe with predictions appended
 
 def randomforest(X_insample, y_insample, X_outsample, y_outsample):
     """ Creates random forests with max_depth 1,2,3,5,10 and random_state=123 """
-    # transform data into count-vectorized and TFIDF-vectorized data
-    X_cv_insample, X_cv_outsample = count_vectorizer(X_insample, X_outsample)
-    X_tfidf_insample, X_tfidf_outsample = tfidf(X_insample, X_outsample)
     # set loop list
     max_depths = [1,2,3,5,10]
     # loop through max depths
     for depth in max_depths:
-        # create forests for count-vectorized and TFIDF-vectorized documents
-        rf_cv = RandomForestClassifier(max_depth=depth, random_state=123)\
-            .fit(X_cv_insample, y_insample.in_actuals)
-        rf_tfidf = RandomForestClassifier(max_depth=depth, random_state=123)\
-            .fit(X_tfidf_insample, y_insample.in_actuals)
+        # create random forest model
+        rf = RandomForestClassifier(max_depth=depth, random_state=123)\
+            .fit(X_insample, y_insample.in_actuals)
         # make predictions in new columns
-        y_insample['cv_rf_depth' + str(depth)] = rf_cv.predict(X_cv_insample)
-        y_outsample['cv_rf_depth' + str(depth)] = rf_cv.predict(X_cv_outsample)
-        y_insample['tfidf_rf_depth' + str(depth)] = rf_tfidf.predict(X_tfidf_insample)
-        y_outsample['tfidf_rf_depth' + str(depth)] = rf_tfidf.predict(X_tfidf_outsample)
+        y_insample['rf_depth' + str(depth)] = rf.predict(X_insample)
+        y_outsample['rf_depth' + str(depth)] = rf.predict(X_outsample)
     
     return y_insample, y_outsample # return dataframe with predictions appended
 
 def logisticregression(X_insample, y_insample, X_outsample, y_outsample):
     """ Creates logistic regressions with random_state=123 """
-    # transform data into count-vectorized and TFIDF-vectorized data
-    X_cv_insample, X_cv_outsample = count_vectorizer(X_insample, X_outsample)
-    X_tfidf_insample, X_tfidf_outsample = tfidf(X_insample, X_outsample)
-    # create logistic regressions for count-vectorized and TFIDF-vectorized documents
-    logit_cv = LogisticRegression(random_state=123)\
-        .fit(X_cv_insample, y_insample.in_actuals)
-    logit_tfidf = LogisticRegression(random_state=123)\
-        .fit(X_tfidf_insample, y_insample.in_actuals)
+    # create logistic regression model
+    logit = LogisticRegression(random_state=123)\
+        .fit(X_insample, y_insample.in_actuals)
     # add columns for predictions
-    y_insample['cv_logit'] = logit_cv.predict(X_cv_insample)
-    y_outsample['cv_logit'] = logit_cv.predict(X_cv_outsample)
-    y_insample['tfidf_logit'] = logit_tfidf.predict(X_tfidf_insample)
-    y_outsample['tfidf_logit'] = logit_tfidf.predict(X_tfidf_outsample)
+    y_insample['logit'] = logit.predict(X_insample)
+    y_outsample['logit'] = logit.predict(X_outsample)
     
     return y_insample, y_outsample # return dataframe with predictions appended
 
 def naivebayes(X_insample, y_insample, X_outsample, y_outsample):
     """ Creates Naive-Bayes with var_smoothing of .001, .01, 10, 100 """
-    # transform data into count-vectorized and TFIDF-vectorized data
-    X_cv_insample, X_cv_outsample = dense_cv(X_insample, X_outsample)
-    X_tfidf_insample, X_tfidf_outsample = dense_tfidf(X_insample, X_outsample)
     # set loop list
     smooth_levels = [.001, .01, 10, 100]
     # loop through smoothing levels
     for smooth_level in smooth_levels:
-        # create naive bayes for count-vectorized and TFIDF-vectorized documents
-        nb_cv = GaussianNB(var_smoothing=smooth_level)\
-            .fit(X_cv_insample, y_insample.in_actuals)
-        nb_tfidf = GaussianNB(var_smoothing=smooth_level)\
-            .fit(X_tfidf_insample, y_insample.in_actuals)
+        # create naive bayes model
+        nb = GaussianNB(var_smoothing=smooth_level)\
+            .fit(X_insample, y_insample.in_actuals)
         # make predictions in new column
-        y_insample['cv_nb_vsmooth' + str(smooth_level)] = nb_cv.predict(X_cv_insample)
-        y_outsample['cv_nb_vsmooth' + str(smooth_level)] = nb_cv.predict(X_cv_outsample)
-        y_insample['tfidf_nb_vsmooth' + str(smooth_level)] = nb_tfidf.predict(X_tfidf_insample)
-        y_outsample['tfidf_nb_vsmooth' + str(smooth_level)] = nb_tfidf.predict(X_tfidf_outsample)
+        y_insample['nb_vsmooth' + str(smooth_level)] = nb.predict(X_insample)
+        y_outsample['nb_vsmooth' + str(smooth_level)] = nb.predict(X_outsample)
     
     return y_insample, y_outsample # return dataframe with preds appended
 
 def knearestneighbors(X_insample, y_insample, X_outsample, y_outsample):
     """ Create KNNs with neighbor counts of 3, 5, 10, 25, 75 """
-    # transform data into count-vectorized and TFIDF-vectorized data
-    X_cv_insample, X_cv_outsample = count_vectorizer(X_insample, X_outsample)
-    X_tfidf_insample, X_tfidf_outsample = tfidf(X_insample, X_outsample)
     # set loop list
     neighbor_counts = [3,5,10,25,75]
     # loop through neighbor counts
     for neighbor_count in neighbor_counts:
-        # create knn models
-        knn_cv = KNeighborsClassifier(n_neighbors=neighbor_count)\
-            .fit(X_cv_insample, y_insample.in_actuals)
-        knn_tfidf = KNeighborsClassifier(n_neighbors=neighbor_count)\
-            .fit(X_tfidf_insample, y_insample.in_actuals)
+        # create knn model
+        knn = KNeighborsClassifier(n_neighbors=neighbor_count)\
+            .fit(X_insample, y_insample.in_actuals)
         # make predictions in new column
-        y_insample['cv_knn_n' + str(neighbor_count)] = knn_cv.predict(X_cv_insample)
-        y_outsample['cv_knn_n' + str(neighbor_count)] = knn_cv.predict(X_cv_outsample)
-        y_insample['tfidf_knn_n' + str(neighbor_count)] = knn_tfidf.predict(X_tfidf_insample)
-        y_outsample['tfidf_knn_n' + str(neighbor_count)] = knn_tfidf.predict(X_tfidf_outsample)
+        y_insample['knn_n' + str(neighbor_count)] = knn.predict(X_insample)
+        y_outsample['knn_n' + str(neighbor_count)] = knn.predict(X_outsample)
     
     return y_insample, y_outsample # return dataframe with preds appended
-
-# -------------------------- the vectorizers -------------------------- #
-
-def count_vectorizer(X_insample, X_outsample):
-    """ Return count-vectorized Pandas Series """
-    # build vectorizer
-    cv = CountVectorizer()
-    # transform data
-    X_cv_insample = cv.fit_transform(X_insample)
-    X_cv_outsample = cv.transform(X_outsample)
-
-    return X_cv_insample, X_cv_outsample # return transformed data
-
-def tfidf(X_insample, X_outsample):
-    """ Return TF-IDF Pandas Series """
-    # build vectorizer
-    tfidf = TfidfVectorizer()
-    # transform data
-    X_tfidf_insample = tfidf.fit_transform(X_insample)
-    X_tfidf_outsample = tfidf.transform(X_outsample)
-
-    return X_tfidf_insample, X_tfidf_outsample # return transformed data
-
-def dense_cv(X_insample, X_outsample):
-    """ Return *dense* count-vectorized Pandas Series """
-    # build vectorizer
-    cv = CountVectorizer()
-    # transform data
-    X_cv_insample = cv.fit_transform(X_insample).todense()
-    X_cv_outsample = cv.transform(X_outsample).todense()
-
-    return X_cv_insample, X_cv_outsample # return transformed data
-
-def dense_tfidf(X_insample, X_outsample):
-    """ Return *dense* TF-IDF Pandas Series """
-    # build vectorizer
-    tfidf = TfidfVectorizer()
-    # transform data
-    X_tfidf_insample = tfidf.fit_transform(X_insample).todense()
-    X_tfidf_outsample = tfidf.transform(X_outsample).todense()
-
-    return X_tfidf_insample, X_tfidf_outsample # return transformed data
